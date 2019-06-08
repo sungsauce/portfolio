36342cd97b09faf06507a238b24c95f3b982111e
Wendy Sung <wendalssung@gmail.com>
2019-06-07 23:20:35 -0400

    fix server port num

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 
 // const {db} = require('./server/db')
 const app = require('./server')
-const PORT = 1337
+const PORT = process.env.PORT || 1337
+
 
 app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
 
